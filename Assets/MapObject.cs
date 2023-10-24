d3acb8bd01f9d830bb267cda5ec4c40ac93a2284
DESKTOP-VN8AUHK\Test <1>
2023-10-24 15:40:03 +0200

    saving to json

---
+++ Assets/MapObject.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapObject : MonoBehaviour
+{
+    public types type;
+    public MapObjects mapObject;
+
+    [Serializable]
+    public struct MapObjects
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public types type;
+
+        public MapObjects(Vector3 pos, Quaternion rot, types name)
+        {
+            this.position = pos;
+            this.rotation = rot;
+            this.type = name;
+        }
+    }
+
+    private void Start()
+    {
+        mapObject = new MapObjects(transform.position, transform.rotation, type);
+    }
+
+    public enum types
+    {
+        StartLine,
+        EndLine,
+        Checkpoint,
+        Bumper,
+        Shooter,
+        Fan,
+        Trampoline,
+        MedCircle,
+        MedFlatRectangle,
+        MedFlatSquare,
+        SmallCircle,
+        SmallFlatSquare,
+        SmallFlatRectangle,
+        SmallHill,
+        SmallLongHill,
+        Void,
+
+
+    }
+
+
+}
