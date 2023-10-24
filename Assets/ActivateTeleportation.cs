375a6a5717592b2c10945b0cc9278983653b668b
Daemon-501 <adrien.mouton@viacesi.fr>
2023-10-24 10:18:51 +0200

    First commit

---
+++ Assets/ActivateTeleportation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class ActivateTeleportation : MonoBehaviour
+{
+
+    //public GameObject LeftTeleportation;
+
+   // public GameObject RightTeleportation;
+
+    //public InputActionProperty LeftActive;
+    //public InputActionProperty RightActive;
+    //Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //LeftTeleportation.SetActive(LeftActive.action.ReadValue<float>() > 0.1f);
+        //RightTeleportation.SetActive(RightActive.action.ReadValue<float>() > 0.1f);
+         
+
+    }
+}
