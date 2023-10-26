9f7205dfff5b0edb531e3c484563927c85b170a5
DESKTOP-VN8AUHK\Test <1>
2023-10-26 16:53:20 +0200

    working lock + haptic

--- Assets/Verrouillage.cs
+++ Assets/Verrouillage.cs
@@ -17,22 +17,11 @@ public class Verrouillage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
-
-
-
-
-
     {
-
-
-
-
-
 
 
 
@@ -40,68 +29,57 @@ public class Verrouillage : MonoBehaviour
         {
             
                 Debug.Log("verrouiller");
-                onVerouillage();
-
-            
-
-           
+                onVerouillage();  
 
         }
 
         if (deverouillage.action.triggered)
         {
-                Debug.Log("verrouiller");
+                Debug.Log("deverouiller");
                 onDeVerouillage();
-
-            
-
-            
-
 
         }
 
 
-
-
-
     }
-
 
     private void onVerouillage()
     {
         
         RaycastHit hit;
 
+
         if (Physics.Raycast(transform.position, transform.forward, out hit))
-            
         {
-                  GameObject objetPointe = hit.collider.gameObject;
-          
+            GameObject objetPointe = hit.collider.gameObject;
+
+            if(objetPointe.GetComponent<XRGrabInteractable>())
+            {
                 XRGrabInteractable interactable = objetPointe.GetComponent<XRGrabInteractable>();
                 objetPointe.tag = "Undeletable";
-            interactable.enabled =false;
-            Renderer render = objetPointe.GetComponentInChildren<MeshRenderer>();
+                interactable.enabled = false;
+                Renderer render = objetPointe.GetComponentInChildren<MeshRenderer>();
 
 
 
 
-            if (render != null && newMaterial != null)
-            {
-                Material[] materials = render.materials; 
-                Material[] newMaterials = new Material[materials.Length + 1]; 
-                for (int i = 0; i < materials.Length; i++)
+                if (render != null && newMaterial != null)
                 {
-                    newMaterials[i] = materials[i]; 
-               }
+                    Material[] materials = render.materials;
+                    Material[] newMaterials = new Material[materials.Length + 1];
+                    for (int i = 0; i < materials.Length; i++)
+                    {
+                        newMaterials[i] = materials[i];
+                    }
 
-                newMaterials[newMaterials.Length - 1] = newMaterial; 
-               render.materials = newMaterials; 
+                    newMaterials[newMaterials.Length - 1] = newMaterial;
+                    render.materials = newMaterials;
+                }
+                else
+                {
+                    Debug.LogWarning("Renderer ou nouveau matériau non définis.");
+                }
             }
-            else
-            {
-               Debug.LogWarning("Renderer ou nouveau matériau non définis.");
-            }
-               
 
         }
 
@@ -119,32 +97,37 @@ public class Verrouillage : MonoBehaviour
         {
             GameObject objetPointe = hit.collider.gameObject;
 
-            XRGrabInteractable interactable = objetPointe.GetComponent<XRGrabInteractable>();
-            interactable.enabled =true;
-            objetPointe.tag = "Untagged";
-
-            Renderer render = objetPointe.GetComponent<MeshRenderer>();
-            if (render != null && materialToRemove != null)
+            if (objetPointe.GetComponent<XRGrabInteractable>())
             {
-                Material[] materials = render.sharedMaterials;
-                List<Material> newMaterials = new List<Material>(materials);
+                XRGrabInteractable interactable = objetPointe.GetComponent<XRGrabInteractable>();
+                interactable.enabled = true;
+                objetPointe.tag = "Untagged";
 
-
-                for (int i = newMaterials.Count - 1; i >= 0; i--)
+                Renderer render = objetPointe.GetComponent<MeshRenderer>();
+                if (render != null && materialToRemove != null)
                 {
-                    if (newMaterials[i] == materialToRemove)
+                    Material[] materials = render.sharedMaterials;
+                    List<Material> newMaterials = new List<Material>(materials);
+
+
+                    for (int i = newMaterials.Count - 1; i >= 0; i--)
                     {
-                        newMaterials.RemoveAt(i);
+                        if (newMaterials[i] == materialToRemove)
+                        {
+                            newMaterials.RemoveAt(i);
+                        }
                     }
+
+
+                    render.sharedMaterials = newMaterials.ToArray();
                 }
-
-
-                render.sharedMaterials = newMaterials.ToArray();
+                else
+                {
+                    Debug.LogWarning("Renderer ou matériau à supprimer non définis.");
+                }
             }
-            else
-            {
-                Debug.LogWarning("Renderer ou matériau à supprimer non définis.");
-            }
+
+            
 
 
         }
